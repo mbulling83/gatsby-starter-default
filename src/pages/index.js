ddfcc520e5751f760840eed71a4e05dcba5d8c46
mbulling83 <42835996+mbulling83@users.noreply.github.com>
2023-03-29 15:21:35 +0100

    Updated home page using GPT

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from "react"
+import { Link } from "gatsby"
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import Seo from '../components/seo'
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  return (
+    <div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1/">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2/">Page 2</Link>
+            </li>
+            <li>
+              <Link to="/page3/">Page 3</Link>
+            </li>
+            <li>
+              <Link to="/page4/">Page 4</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <header>
+          <Link to="/">My Gatsby Site</Link>
+        </header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/about/">About Us</Link>
+            </li>
+            <li>
+              <Link to="/contact/">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h1>Welcome to my Gatsby site!</h1>
+        <p>This is the index page.</p>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
-
-export default IndexPage
+  )
+}
